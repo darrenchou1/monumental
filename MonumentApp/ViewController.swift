f5126d510100680abd76b2d3251414644676f7af
Alvin Tsui <awtsui@uci.edu>
2020-01-10 21:19:58 -0800

    Created App
    Just created

---
+++ MonumentApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MonumentApp
+//
+//  Created by Alvin Tsui on 1/10/20.
+//  Copyright © 2020 SBHacks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
